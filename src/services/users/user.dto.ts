f547ac5fd11c0c8019cad6f905791ff9609351da
Barakat Turki <barakat.turki@gmail.com>
2020-04-29 21:48:06 +0200

    reafactor(services): remove dummy services

--- src/services/users/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-export default class createUserDto {
-  @IsString()
-  public name!: string;
-  @IsString()
-  public email!: string;
-  @IsString()
-  public password!: string;
-}
